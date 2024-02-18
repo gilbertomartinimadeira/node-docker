10b0a4b3d9a19d00369a121f2080c889482eb422
Gilberto Martini Madeira <gilberto.madeira@outlook.com>
2024-02-18 11:23:25 -0300

    Added basic connection retry

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/c
 
 const app = express();
 
-mongoose
-    .connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
+const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+    mongoose
+    .connect(mongoUrl)
     .then( () => console.log("Successfully Connected to mongo database"))
-    .catch( e => console.log(e));
+    .catch( e => { 
+        console.log(e);
+        setTimeout(connectWithRetry, 5000);
+    });
+};
+// for 30 seconds, it retries automatically
+
 
 
 
@@ -15,7 +24,7 @@ app.get('/',    (req,res) => {
     res.send('<h2>Hi There in development!!!</h2>');
 });
 
-const port= process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listening on port ${port}`));
 
