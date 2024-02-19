1e5727fa2bfacc53d739e5acf7b04d3d31b284ba
Gilberto Martini Madeira <gilberto.madeira@outlook.com>
2024-02-19 18:18:43 -0300

    Added bcrypt for auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,17 @@
 const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+
 
 exports.signUp = async (req, res) => {
+
+    const {username, password} = req.body;
+    const hashPassword = await bcrypt.hash(password, 12);
+
   try {
-    const newUser = await User.create(req.body)
+    const newUser = await User.create({
+        username: username,
+        password: hashPassword
+    })
     res.status(201).send({
         status : "success",
         data: {
